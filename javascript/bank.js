63f674a0c078742ab76c351aa4276b2370394356
marufbellah <mdmaruf5511507@gmail.com>
2023-08-27 23:25:37 +0600

    manage those code

--- javascript/bank.js
+++ javascript/bank.js
@@ -1,17 +1,17 @@
-document.getElementById('diposite-button').addEventListener('click', function(){
-    const innerText = document.getElementById("diposite-input")
+document.getElementById("diposite-button").addEventListener("click", function () {
+    const innerText = document.getElementById("diposite-input");
     const inputValue = innerText.value;
 
-    const diposite =document.getElementById("diposite-total");
+    const diposite = document.getElementById("diposite-total");
     const dipositeTotal = diposite.innerText = inputValue;
-    const stringToNumberDiposit = parseFloat(dipositeTotal)
 
-
-    const balance =document.getElementById('balance-total');
-    const balanceShow =balance.innerText;
-    const stringToNumberBalanc = parseFloat(balanceShow)
-    console.log(typeof balanceShow);
-
-    const total = stringToNumberBalanc + stringToNumberDiposit;
-    document.getElementById('balance-total').innerText = total;
-});
+     const stringToNumberDiposit = parseFloat(dipositeTotal);
+     const balance = document.getElementById("balance-total")
+     const balanceShow = balance.innerText
+     const stringToNumberBalanc = parseFloat(balanceShow);
+    
+     const total = stringToNumberBalanc + stringToNumberDiposit;
+     document.getElementById("balance-total").innerText = total;
+            
+        
+  });
